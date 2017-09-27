7531eebbaebd40c722e6f8ddb9d8fc63b2a31826
IWP <mricky.it@gmail.com>
2017-09-26 22:32:36 -0700

    Taxi App

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class Main extends React.Component {	
+	render(){
+		return(
+			<View>
+				<Text>Taxi from Main</Text>
+			</View>
+		);
+
+	}
+}
+
+export default Main;
